e64e79d4eaf0f0749b301610b7ea3dd12be2d1d9
dotneteeer <447599489marat@gmail.com>
2025-05-04 11:43:12 +0300

    Unit tests updated

--- QuestionService.Tests/UnitTests/Factories/ViewServiceFactory.cs
+++ QuestionService.Tests/UnitTests/Factories/ViewServiceFactory.cs
@@ -1,7 +1,9 @@
+using Microsoft.Extensions.Options;
 using QuestionService.Application.Services;
 using QuestionService.Domain.Entities;
 using QuestionService.Domain.Interfaces.Repository;
 using QuestionService.Domain.Interfaces.Service;
+using QuestionService.Domain.Settings;
 using QuestionService.Tests.Configurations;
 using QuestionService.Tests.UnitTests.Configurations;
 using StackExchange.Redis;
@@ -11,17 +13,20 @@ namespace QuestionService.Tests.UnitTests.Factories;
 internal class ViewServiceFactory
 {
     private readonly IViewService _viewService;
+    public readonly BusinessRules BusinessRules = BusinessRulesConfiguration.GetBusinessRules();
 
     public readonly IBaseRepository<Question> QuestionRepository =
         MockRepositoriesGetters.GetMockQuestionRepository().Object;
 
     public readonly IDatabase RedisDatabase = RedisDatabaseConfiguration.GetRedisDatabaseConfiguration();
+    public readonly IBaseRepository<View> ViewRepository = MockRepositoriesGetters.GetMockViewRepository().Object;
 
     public ViewServiceFactory(IDatabase? redisDatabase = null)
     {
         if (redisDatabase != null) RedisDatabase = redisDatabase;
 
-        _viewService = new ViewService(RedisDatabase, QuestionRepository);
+        _viewService = new ViewService(RedisDatabase, QuestionRepository, ViewRepository,
+            new OptionsWrapper<BusinessRules>(BusinessRules));
     }
 
     public IViewService GetService()
