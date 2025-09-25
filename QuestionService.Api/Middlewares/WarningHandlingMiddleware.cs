c4879a164511cd96ad7402f7bd29d3d639e5d5eb
dotneteeer <447599489marat@gmail.com>
2025-09-25 20:43:49 +0300

    Enhance WarningHandlingMiddleware to handle gRPC requests and improve response coping logic

--- QuestionService.Api/Middlewares/WarningHandlingMiddleware.cs
+++ QuestionService.Api/Middlewares/WarningHandlingMiddleware.cs
@@ -1,5 +1,4 @@
 using System.Globalization;
-using System.Net;
 using Newtonsoft.Json;
 using QuestionService.Application.Enum;
 using QuestionService.Application.Resources;
@@ -12,12 +11,19 @@ public class WarningHandlingMiddleware(ILogger logger, RequestDelegate next)
 {
     public async Task InvokeAsync(HttpContext httpContext)
     {
+        // Checks if request is gRPC
+        if (httpContext.Request.ContentType?.StartsWith("application/grpc") == true)
+        {
+            await next(httpContext);
+            return;
+        }
+
         // Save the original response body stream
         var originalResponseBody = httpContext.Response.Body;
 
         try
         {
-            // Create am empty temporary memory stream to intercept the response and to read it later
+            // Create an empty temporary memory stream to intercept the response and to read it later
             using var swapStream = new MemoryStream();
             // Redirect response output from httpContext.Response.Body to swapStream
             httpContext.Response.Body = swapStream;
@@ -26,7 +32,7 @@ public class WarningHandlingMiddleware(ILogger logger, RequestDelegate next)
 
             swapStream.Seek(0, SeekOrigin.Begin);
 
-            if (httpContext.Response.StatusCode == (int)HttpStatusCode.BadRequest)
+            if (httpContext.Response.StatusCode == StatusCodes.Status400BadRequest)
             {
                 // Read the obtained body in swapStream
                 var responseBody = await new StreamReader(swapStream).ReadToEndAsync();
@@ -42,7 +48,7 @@ public class WarningHandlingMiddleware(ILogger logger, RequestDelegate next)
             }
 
             // Copy the intercepted response back to the original response stream
-            await swapStream.CopyToAsync(originalResponseBody);
+            if (CanCopy(httpContext.Response, originalResponseBody)) await swapStream.CopyToAsync(originalResponseBody);
         }
         finally
         {
@@ -59,5 +65,16 @@ public class WarningHandlingMiddleware(ILogger logger, RequestDelegate next)
 
 
         return errorMessage?.TrimEnd('.');
+    }
+
+    private static bool CanCopy(HttpResponse response, Stream originalResponseBody)
+    {
+        if (!originalResponseBody.CanWrite) return false;
+
+        return response.StatusCode switch
+        {
+            >= 100 and < 200 or StatusCodes.Status204NoContent or StatusCodes.Status304NotModified => false,
+            _ => true
+        };
     }
 }
