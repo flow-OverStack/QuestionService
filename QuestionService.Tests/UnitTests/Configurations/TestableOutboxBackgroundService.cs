5fea3b2e24d51259dae9dd590e87f74053931227
dotneteeer <447599489marat@gmail.com>
2026-04-20 20:38:54 +0300

    Move `TestableOutboxBackgroundService` to unit tests

--- QuestionService.Tests/UnitTests/Configurations/TestableOutboxBackgroundService.cs
+++ QuestionService.Tests/UnitTests/Configurations/TestableOutboxBackgroundService.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using QuestionService.Outbox;
 using Serilog;
 
-namespace QuestionService.Tests.FunctionalTests.Configurations.TestServices;
+namespace QuestionService.Tests.UnitTests.Configurations;
 
 internal class TestableOutboxBackgroundService(ILogger logger, IServiceScopeFactory scopeFactory)
     : OutboxBackgroundService(logger, scopeFactory)
